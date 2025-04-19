ac5c4e21a0cf22bc33cdce92756c5ae78099c774
Louca Morales <lomorale@c3r5s1.42lausanne.ch>
2025-04-19 23:07:55 +0200

    repair cd

--- src/execution/exec_built_02.c
+++ src/execution/exec_built_02.c
@@ -6,7 +6,7 @@
 /*   By: lomorale <lomorale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:37:39 by lomorale          #+#    #+#             */
-/*   Updated: 2025/04/19 23:01:11 by lomorale         ###   ########.fr       */
+/*   Updated: 2025/04/19 23:06:13 by lomorale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,6 @@ void	exec_exit(void)
 	free_data(tmp_data);
 	exit(errno);
 }
-/*
-int	adapt_cd(t_cmd *cmd, t_list **env_list)
-{
-	char	buffer_pwd[1024];
-	char	buffer_oldpwd[1024];
-	if (!cmd->cmd_args[1])
-	{
-		write_env_list(getcwd(buffer_oldpwd, sizeof(buffer_oldpwd)),
-			"OLDPWD=", env_list);
-		chdir(give_var_env_list("HOME=", *env_list));
-		write_env_list(getcwd(buffer_pwd, sizeof(buffer_pwd)),
-			"PWD=", env_list);
-	}
-	else
-		exec_cd(cmd->cmd_args[1], env_list);
-	return (errno);
-}
-*/
 
 int	exec_cd(t_cmd *cmd, t_list **env_list)
 {
