5b27ff6ec186ad56f69d32d4c82317c41459c192
Oscar Druke Santana <odruke-s@c3r6s2.42lausanne.ch>
2025-04-24 23:00:54 +0200

    fixed SIGQUIT segfault on exit

--- src/execution/exec_built/exec_built_in_exit.c
+++ src/execution/exec_built/exec_built_in_exit.c
@@ -19,21 +19,24 @@ void	exec_exit(char **cmd_args)
 	int		exit_status;
 
 	exit_status = EXIT_SUCCESS;
-	i = -1;
-	if (cmd_args[1])
+	if (cmd_args)
 	{
-		while (cmd_args[1][++i])
-			if (!ft_isdigit(cmd_args[1][i]))
-				break ;
-		if (!cmd_args[1][i])
-			exit_status = ft_atoi(cmd_args[1]);
-		else
-			exit_status = 2;
+		i = -1;
+		if (cmd_args[1])
+		{
+			while (cmd_args[1][++i])
+				if (!ft_isdigit(cmd_args[1][i]))
+					break ;
+			if (!cmd_args[1][i])
+				exit_status = ft_atoi(cmd_args[1]);
+			else
+				exit_status = 2;
+		}
+		ft_printf_fd(2, "exit\n");
+		if (cmd_args[1] && exit_status == 2)
+			ft_printf_fd(2, "Minishell: exit: %s:  numeric argument required\n",
+					cmd_args[1]);
 	}
-	ft_printf_fd(2, "exit\n");
-	if (cmd_args[1] && exit_status == 2)
-		ft_printf_fd(2, "Minishell: exit: %s:  numeric argument required\n",
-				cmd_args[1]);
 	tmp_data = recover_data_address(NULL);
 	free_data(tmp_data);
 	exit(exit_status);
--- src/main/main.c
+++ src/main/main.c
@@ -67,8 +67,9 @@ int	main(int ac, char **av, char **env)
 		signal(SIGQUIT, SIG_IGN);
 		data->input = readline("Minishell $ ");
 		if (!data->input)
-			exec_exit();
+			exec_exit(NULL);
 		add_history(data->input);
+
 		parsing(data);
 		create_cmd_block(data->token_list, &data->cmd_list);
 		execution(data);
