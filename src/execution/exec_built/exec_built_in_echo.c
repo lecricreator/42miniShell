8d78b6f500f0eb90df101b3f8e164351ae799a8f
Louca Morales <lomorale@c1r16s2.42lausanne.ch>
2025-04-25 15:55:14 +0200

    echo work correctly FUCK

--- src/execution/exec_built/exec_built_in_echo.c
+++ src/execution/exec_built/exec_built_in_echo.c
@@ -6,11 +6,55 @@
 /*   By: lomorale <lomorale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 15:35:45 by lomorale          #+#    #+#             */
-/*   Updated: 2025/04/25 11:55:08 by lomorale         ###   ########.fr       */
+/*   Updated: 2025/04/25 15:54:34 by lomorale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	is_n_flag(char *str)
+{
+	str+ = 2;
+	while (*str)
+	{
+		if (*str == 'n')
+			str++;
+		else
+			break ;
+	}
+	if (*str == '\0')
+		return (TRUE);
+	return (FALSE);
+}
+
+int	exec_echo(char **cmd_args)
+{
+	int	i;
+	int	line_break;
+
+	i = 1;
+	line_break = TRUE;
+	while (cmd_args[i])
+	{
+		if (ft_strncmp(cmd_args[i], "-n", 2) == 0 && is_n_flag(cmd_args[i]))
+			line_break = FALSE;
+		else
+		{
+			break ;
+		}
+		i++;
+	}
+	if (cmd_args[i])
+	{
+		ft_printf_fd(1, "%s", cmd_args[i]);
+		i++;
+		while (cmd_args[i])
+			ft_printf_fd(1, " %s", cmd_args[i++]);
+	}
+	if (line_break)
+		ft_printf_fd(1, "\n");
+	return (0);
+}
 
 // int	exec_echo_write(char **cmd_args, int flags, int i)
 // {
@@ -45,51 +89,50 @@
 // 	return (0);
 // }
 
+// int	exec_echo(char **cmd_args)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		flags;
+// 	int		return_line;
+// 	int		first_write;
 
-int	exec_echo(char **cmd_args)
-{
-	int		i;
-	int		j;
-	int		flags;
-	int		return_line;
-	int		first_write;
-
-	i = 0;
-	j = 0;
-	flags = 1;
-	return_line = 1;
-	first_write = 1;
-	while (cmd_args[++i])
-	{
-		if (flags && cmd_args[i][0] == '-')
-		{
-			while (cmd_args[i][++j] && flags == 1)
-			{
-				if (cmd_args[i][j] == 'n' || cmd_args[i][j] == '\0')
-					flags = 1;
-				else
-				{
-					ft_printf_fd(1, "%s", cmd_args[i]);
-					flags = 0;
-					return_line = 0;
-					first_write = 0;
-				}
-			}
-			if (i == 1 && flags)
-				return_line = 0;
-		}
-		else
-		{
-			if (first_write)
-			{
-				ft_printf_fd(1, "%s", cmd_args[i]);
-				first_write = 0;
-			}
-			else
-				ft_printf_fd(1, " %s", cmd_args[i]);
-		}
-	}
-	if (return_line == 1)
-		ft_printf_fd(1, "\n");
-	return (0);
-}
+// 	i = 0;
+// 	j = 0;
+// 	flags = 1;
+// 	return_line = 1;
+// 	first_write = 1;
+// 	while (cmd_args[++i])
+// 	{
+// 		if (flags && cmd_args[i][0] == '-')
+// 		{
+// 			while (cmd_args[i][++j] && flags == 1)
+// 			{
+// 				if (cmd_args[i][j] == 'n' || cmd_args[i][j] == '\0')
+// 					flags = 1;
+// 				else
+// 				{
+// 					ft_printf_fd(1, "%s", cmd_args[i]);
+// 					flags = 0;
+// 					return_line = 0;
+// 					first_write = 0;
+// 				}
+// 			}
+// 			if (i == 1 && flags)
+// 				return_line = 0;
+// 		}
+// 		else
+// 		{
+// 			if (first_write)
+// 			{
+// 				ft_printf_fd(1, "%s", cmd_args[i]);
+// 				first_write = 0;
+// 			}
+// 			else
+// 				ft_printf_fd(1, " %s", cmd_args[i]);
+// 		}
+// 	}
+// 	if (return_line == 1)
+// 		ft_printf_fd(1, "\n");
+// 	return (0);
+// }
